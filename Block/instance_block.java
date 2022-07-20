d4c488c9ce2dc790fc7bcbb9042ad521eba23173
Gyan prakash kushwaha <103535168+gyan094@users.noreply.github.com>
2022-07-21 05:20:58 +0530

    Add files via upload

---
+++ Block/instance_block.java
@@ -0,0 +1,17 @@
+package Block;
+
+public class instance_block {
+    /* instance_vs_static()
+    {
+        System.out.println("Gyan");
+    }
+  */{
+        //instance variable
+        System.out.println("gyan");  //faster work in defult constructor
+    }
+
+    public static void main(String[] args) {
+        instance_block r=new instance_block();
+
+    }
+}
---
+++ Block/static_block.java
@@ -0,0 +1,18 @@
+package Block;
+
+public class static_block {
+
+
+        /* instance_vs_static()
+        {
+            System.out.println("Gyan");
+        }
+      */
+        static {
+            //static block
+            System.out.println("gyan");  //faster work in defult constructor
+        }
+
+        public static void main(String[] args) {
+        }
+}
